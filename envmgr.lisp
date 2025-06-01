578d7eb6c550a2f5e54b81c48f6bb5e3f5ba7dd9
Edwin Lundmark <edwin.lundmark@icloud.com>
2025-06-01 12:29:46 +0200

    Documentation for planned usage

--- envmgr.lisp
+++ envmgr.lisp
@@ -5,10 +5,20 @@
 (defun handle-alias (original new)
   (format t "'~a' is now '~a'~%" original new))
 
+
 (defun main (argv)
+  "Usage: envmgr [options]
+
+Options:
+  -a, --alias <command> <replacement>
+  -v, --variable <variable> <value>
+  -p, --print
+  -i, --interactive"
+  
   (with-open-file (file "aliases")
     (let ((content (uiop:read-file-lines file)))
       (parse-args argv
-	      ("--alias" #'handle-alias)
-      	      ("--print" (lambda ()
-			   (format t "~a~%" (first content))))))))
+		  ("--help" (lambda () (format t "~a" (documentation #'main 'function))))
+		  ("--alias" #'handle-alias)
+		  ("--print" (lambda ()
+			       (format t "~a~%" (first content))))))))
