cae1e86dbc43f28a71f55672119fb9efef681281
Edwin Lundmark <edwin.lundmark@icloud.com>
2025-06-01 14:29:15 +0200

    Started on general function to change value instead of handle-alias

--- envmgr.lisp
+++ envmgr.lisp
@@ -2,8 +2,19 @@
 
 (in-package #:envmgr)
 
-(defun handle-alias (original new)
-  (format t "'~a' is now '~a'~%" original new))
+(defun change-value (original new type content lines)
+  (let ((found-value
+	  (find-if #'(lambda (pair) (and
+				     (string= (first pair) original)
+				     (eq (third pair) type)))
+		   content))
+	(found-line
+	  (find-if #'(lambda (line)
+		       (numberp (or
+				 (search (uiop:strcat original "=") line)
+				 (search (uiop:strcat original "'=") line))))
+		   lines)))
+    (format t "~a ~a ~a ~%" found-line found-value new)))
 
 (defun handle-print (content)
   (iter (for pair in content)
@@ -12,15 +23,16 @@
 
 ;; TODO: import clj-arrows instead
 (defun handle-file-line (line)
-  (let ((str (coerce line 'string)) (type nil))
-    (as-> (uiop:split-string str) $
-	  (progn
-	    (setf type (first $))
-	    (cdr $))
-	  (apply #'uiop:strcat $)
-	  (uiop:split-string $ :separator "=")
-	  (mapcar #'(lambda (s) (remove #\' s)) $)
-	  (append $ (list (read-from-string type))))))
+  (if (> (length line) 0)
+      (let ((str (coerce line 'string)) (type nil))
+	(as-> (uiop:split-string str) $
+	      (progn
+		(setf type (first $))
+		(cdr $))
+	      (apply #'uiop:strcat $)
+	      (uiop:split-string $ :separator "=")
+	      (mapcar #'(lambda (s) (remove #\' s)) $)
+	      (append $ (list (read-from-string type)))))))
 
 (defun main (argv)
   "Usage: envmgr [options]
@@ -32,8 +44,8 @@ Options:
   -i, --interactive"
   
   (with-open-file (file "aliases")
-    (let ((content (mapcar #'handle-file-line (uiop:read-file-lines file))))
+    (let* ((lines (uiop:read-file-lines file)) (content (mapcar #'handle-file-line lines)))
       (parse-args argv
 		  ("--help" (lambda () (format t "~a" (documentation #'main 'function))))
-		  ("--alias" #'handle-alias)
-		  ("--print" (lambda () (handle-print content)))))))
+		  ("--alias" (lambda (original new) (change-value original new 'alias content lines))
+		  ("--print" (lambda () (handle-print content))))))))
